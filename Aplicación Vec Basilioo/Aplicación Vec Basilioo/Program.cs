e4ab930b94093d36b2dd1808a3b848e7ab43e901
emilibasilio <emilinicolebasilio.garcia@gmail.com>
2021-06-08 06:04:28 -0600

    Inicio de proyecto consola #

---
+++ Aplicación Vec Basilioo/Aplicación Vec Basilioo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicación_Vec_Basilioo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] dato = new char[10];
+
+            for(int i=0; i<=9; i++)
+            {
+                Console.WriteLine("i: " + i);
+                Console.Write("Ingrese una letra: ");
+                dato[i] = char.Parse(Console.ReadLine());
+                
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
