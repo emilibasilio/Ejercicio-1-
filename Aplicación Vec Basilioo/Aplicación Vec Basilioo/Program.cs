b5f2ad31bfb9b7e3a73f1c188826b98f2e2fc29d
emilibasilio <emilinicolebasilio.garcia@gmail.com>
2021-06-08 14:37:00 -0600

    Avance de aplicación

--- Aplicación Vec Basilioo/Aplicación Vec Basilioo/Program.cs
+++ Aplicación Vec Basilioo/Aplicación Vec Basilioo/Program.cs
@@ -11,15 +11,66 @@ namespace Aplicación_Vec_Basilioo
         static void Main(string[] args)
         {
             char[] dato = new char[10];
+            char buscar;
+            char p = 's';
+            int e;
 
-            for(int i=0; i<=9; i++)
+
+            while (p == 's')
             {
-                Console.WriteLine("i: " + i);
-                Console.Write("Ingrese una letra: ");
-                dato[i] = char.Parse(Console.ReadLine());
-                
-            }
+              Console.Clear();
+              Console.WriteLine("Que opción desea realizar: \n1. Ingresar valores \n2. Mostrar valores \n3. Buscar valores ");
+              e = int.Parse(Console.ReadLine());
 
+                Console.Clear();
+                if (e==1)
+                {
+
+                 for (int i=0; i<=9; i++)
+                 {
+                    Console.WriteLine("i: " + i);
+                    Console.Write("Ingrese una letra: ");
+                    dato[i] = char.Parse(Console.ReadLine());
+
+                        
+                 }
+                    Console.ReadKey();
+                }
+
+               // *Console.WriteLine("------------------" );
+            
+                if (e==2)
+                {
+                    Console.Clear();
+                    for (int x=0; x<=9; x++)
+                 {
+                      Console.WriteLine(dato[x]);
+
+                 }
+                    Console.ReadKey();
+                }
+                //* Console.WriteLine("------------------");
+
+                else if(e==3)
+                {
+                    Console.Clear();
+                  Console.Write("Ingrese caracter a buscar: ");
+                  buscar = char.Parse(Console.ReadLine());
+
+                 for(int z=0; z<=9; z++)
+                 {
+                     if(dato[z]==buscar)
+                     {
+                         Console.Write("dato: "+buscar+" encontrado" );
+                     }
+                 }
+                    Console.ReadKey();
+                }
+                    Console.Clear();
+                    Console.WriteLine("Desea Continuar ['s/n']");
+                    p = char.Parse(Console.ReadLine());
+
+            }
             Console.ReadKey();
 
 
